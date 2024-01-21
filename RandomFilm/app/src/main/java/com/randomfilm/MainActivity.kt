cf40910f4db8b0e8f13342f4852dc0f7da26b474
Sergei <106600613+MS-17@users.noreply.github.com>
2024-01-21 11:48:23 +0800

    add views

--- RandomFilm/app/src/main/java/com/randomfilm/MainActivity.kt
+++ RandomFilm/app/src/main/java/com/randomfilm/MainActivity.kt
@@ -5,9 +5,6 @@ import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import kotlinx.serialization.*
 import kotlinx.serialization.json.*
 
 class MainActivity : ComponentActivity() {
@@ -21,33 +18,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // views to fill later
         val resultTextView = this.findViewById<TextView>(R.id.result_text_view)
+        val filmTitleTextView = findViewById<TextView>(R.id.title)
+        val descTextView = findViewById<TextView>(R.id.description)
+        val ratingTextView = findViewById<TextView>(R.id.rating)
+        val yearTextView = findViewById<TextView>(R.id.year)
+        val starringTextView = findViewById<TextView>(R.id.starring)
 
-        val filmsList = resources.getStringArray(R.array.films)
+        //val filmsList = resources.getStringArray(R.array.films)
 
-        // parse films json
-        val gson = Gson()
+        // parse json with kotlinx.serialization
         val filmsJson = resources.openRawResource(R.raw.films)
                              .bufferedReader(Charsets.UTF_8)
                              .use { it.readText() }
         Log.d("My info", filmsJson)
-        // kotlinx.serialization
-        val d = Json.decodeFromString<Map<String, List<Film>>>(filmsJson)
-        Log.d("My info", d.toString())
-
+        val filmsList = Json.decodeFromString<Map<String, Array<Film>>>(filmsJson)["films"]
+        Log.d("My info", filmsList.toString())
 
         // buttons
         val resetBtn = findViewById<Button>(R.id.reset_button)
         resetBtn.setOnClickListener {
             Log.d("My Info", "Restarting")
             findViewById<TextView>(R.id.result_text_view).text = "Let's start again"
+            filmTitleTextView.text = "Title: -"
+            descTextView.text = "Description: -"
+            ratingTextView.text = "Rating: -"
+            yearTextView.text = "Year: -"
+            starringTextView.text = "Starring: -"
             counter = 0
             usedIndices.clear()
         }
 
         val submitBtn = findViewById<Button>(R.id.submit_button)
         submitBtn.setOnClickListener {
-            if (counter == filmsList.size) {
+            if (counter == filmsList!!.size) {
                 resultTextView.text = "No films left, reset"
             } else {
                 Log.d("My Info", "Generate indeces")
@@ -57,7 +62,15 @@ class MainActivity : ComponentActivity() {
                     idx = (filmsList.indices).random()
                 }
                 Log.d("My Info", "Index: $idx")
-                resultTextView.text = filmsList[idx]
+                val f = filmsList[idx]
+                resultTextView.text = "Films"
+                filmTitleTextView.text = "Title: " + f.title
+                descTextView.text = "Description: " + f.description
+                ratingTextView.text = "Rating: " + f.rating.toString()
+                yearTextView.text = "Year: " + f.year.toString()
+                starringTextView.text = "Starring: " + f.starring.toString()
+                                                                 .replace("[", "")
+                                                                 .replace("]", "")
                 usedIndices.add(idx)
                 counter++
             }
