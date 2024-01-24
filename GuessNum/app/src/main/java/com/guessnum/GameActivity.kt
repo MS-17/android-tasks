5d30b718da849740c708d0af0afb119391cec2b4
Sergei <106600613+MS-17@users.noreply.github.com>
2024-01-24 17:49:27 +0800

    pass data between intents

--- GuessNum/app/src/main/java/com/guessnum/GameActivity.kt
+++ GuessNum/app/src/main/java/com/guessnum/GameActivity.kt
@@ -1,11 +1,18 @@
 package com.guessnum
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 
 class GameActivity: ComponentActivity() {
+    var begin: Int = 0
+    var end: Int = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game_activity)
+        begin = intent.getIntExtra("begin", 0)
+        end = intent.getIntExtra("end", 100)
+        Log.d("My info", "begin = $begin, end = $end")
     }
 }
