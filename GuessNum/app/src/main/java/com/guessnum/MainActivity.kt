9bd957ae3eaae0007d8a43b4080b54c9cad38197
Sergei <106600613+MS-17@users.noreply.github.com>
2024-01-24 17:30:30 +0800

    add main activity logic

--- GuessNum/app/src/main/java/com/guessnum/MainActivity.kt
+++ GuessNum/app/src/main/java/com/guessnum/MainActivity.kt
@@ -2,13 +2,26 @@ package com.guessnum
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val gameActivity = Intent(this, GameActivity::class.java)
+        findViewById<Button>(R.id.guess_button).setOnClickListener {
+            onGuessBtnClick()
+        }
+    }
 
+    fun onGuessBtnClick() {
+        val gameActivity = Intent(this, GameActivity::class.java)
+        val begin = findViewById<EditText>(R.id.begin).text.toString().toInt()
+        val end = findViewById<EditText>(R.id.end).text.toString().toInt()
+        gameActivity.putExtra("begin", begin)
+        gameActivity.putExtra("end", end)
+        startActivity(gameActivity)
     }
 }
