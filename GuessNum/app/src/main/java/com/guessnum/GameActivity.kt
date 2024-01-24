ff40a34eb4f99a3897118b030f1b608b33266256
Sergei <106600613+MS-17@users.noreply.github.com>
2024-01-24 12:02:16 +0800

    add game activity

--- GuessNum/app/src/main/java/com/guessnum/GameActivity.kt
+++ GuessNum/app/src/main/java/com/guessnum/GameActivity.kt
@@ -1,14 +1,11 @@
 package com.guessnum
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 
-class MainActivity : ComponentActivity() {
+class GameActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val gameActivity = Intent(this, GameActivity::class.java)
-
+        setContentView(R.layout.game_activity)
     }
 }
