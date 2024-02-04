e486ff0f395b2aa6b5c5818a8790bf3e2b870f9a
Sergei <106600613+MS-17@users.noreply.github.com>
2024-02-04 12:45:02 +0800

    ColorTiles: change layout, add onTileClick logic

--- ColorTiles/app/src/main/java/com/colortiles/MainActivity.kt
+++ ColorTiles/app/src/main/java/com/colortiles/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.colortiles
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.activity.ComponentActivity
+import androidx.core.content.ContextCompat
+
+data class Coordinates(val x: Int, val y: Int)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,8 +15,27 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.main_activity)
     }
 
-    fun onTileClick(view: View) {
+    private fun getCoordinatesFromString(s: String): Coordinates {
+        Log.d("My info", "Chosen tile: $s")
+        return Coordinates(s[0].toInt(), s[1].toInt())
+    }
 
+    private fun changeTilesColor(view: View) {
+        Log.d("My info", "Changing tiles color")
+        val redColor = ContextCompat.getColor(this, R.color.red)
+        val greenColor = ContextCompat.getColor(this, R.color.green)
+        val drawable = view.background as ColorDrawable
+        if (drawable.color == redColor) {
+            view.setBackgroundColor(greenColor)
+        } else {
+            view.setBackgroundColor(redColor)
+        }
+        Log.d("My info", "Background after changing color")
+    }
+
+    fun onTileClick(view: View) {
+        val coordinates = getCoordinatesFromString(view.tag.toString())
+        changeTilesColor(view)
     }
 }
 
